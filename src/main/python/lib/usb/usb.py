2faab2a0945034f2f916f52fdd637b86ed2581e8
Arya Habibi <5auru5@users.noreply.github.com>
2023-06-25 21:05:02 -0500

    Adding first iteration of usb connector

--- src/main/python/lib/usb/usb.py
+++ src/main/python/lib/usb/usb.py
@@ -4,12 +4,12 @@ import subprocess
 from dataclasses import dataclass
 
 
-@dataclass
+@dataclass(init=True)
 class UsbDevice():
     name : str
     device_id : str
     location : str
-    
+
     
 class UsbConnector:
     device_list : list[UsbDevice] = []
@@ -20,12 +20,21 @@ class UsbConnector:
         
              
     def _initalize_usb_devices(self):
-        pass
+        device_re = re.compile(b"Bus\s+(?P<bus>\d+)\s+Device\s+(?P<device>\d+).+ID\s(?P<id>\w+:\w+)\s(?P<tag>.+)$", re.I)
+        df = subprocess.check_output("lsusb")
+        for i in df.split(b'\n'):
+            if i:
+                info = device_re.match(i)
+                if info:
+                    dinfo = info.groupdict()
+                    dinfo['device'] = '/dev/bus/usb/%s/%s' % (dinfo.pop('bus'), dinfo.pop('device'))
+                    self.device_list.append(UsbDevice(location=dinfo.get('device'), name=dinfo.get('tag'), device_id=dinfo.get('id')))
         
-    def getDevices(self):
-        if not self.device_list:
+    def getDevices(self) -> list[UsbDevice]:
+        if not self.device_list or self.is_cached is True:
             self._initalize_usb_devices()
             self.is_cached = True
+        return self.device_list
         
         
 class UsbConnection():
@@ -35,6 +44,22 @@ class UsbConnection():
     def __init__(self):
         self.usb_connection = UsbConnector()
         
-    def connect(self):
-        if self.current_device is None:
-            pass
+    def isConnected(self) -> bool:
+        False if self.current_device == None else True 
+        
+    def connect(self, device_id: str):
+        if not self.isConnected():
+            print("Already Connected!")
+        try:
+            self.current_device = next((device_id for id in self.usb_connection.getDevices() if device_id == id.device_id))
+        except Exception:
+            raise KeyError("Could not find selected device! Try to connect again")
+        
+    
+    def disconnect(self):
+        if self.current_device:
+            self.current_device = None
+        print("No Device connected!")
+        
+        
+            
